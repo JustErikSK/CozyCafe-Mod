27d67c807e05f2ad6700df70ceb0bce8b356c26d
justeriksk <sebo.erik@gmail.com>
2025-07-23 22:44:07 +0200

    Initial commit

---
+++ src/main/java/net/withrage/CozyCafe.java
@@ -0,0 +1,16 @@
+package net.withrage;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CozyCafe implements ModInitializer {
+	public static final String MOD_ID = "cozycafe";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/withrage/CozyCafeClient.java
@@ -0,0 +1,10 @@
+package net.withrage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CozyCafeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
