5331cc7c3b415dc87ac7334c575a25a64b3a9987
justeriksk <sebo.erik@gmail.com>
2025-08-01 17:21:00 +0200

    created a ModFoodComponents class, all items have the same modifiers for now

--- src/main/java/net/withrage/item/ModFoodComponents.java
+++ src/main/java/net/withrage/item/ModFoodComponents.java
@@ -5,7 +5,189 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent APPLE_JAM = new FoodComponent.Builder()
+    public static final FoodComponent APPLE_JUICE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent APPLE_PIE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent BERRY_CUPCAKE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent CHICKEN_MUSHROOM_PIE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent CHIPS = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent CHOCOLATE_CUPCAKE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent CHOCOLATE_DONUT = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent CHOCOLATE_MILK = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent FRIES = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent GILDED_MELONADE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent GLOWBERRY_DONUT = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent GLOWBERRY_ICE_CREAM = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent GLOWBERRY_JUICE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent GLOWBERRY_PIE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent JELLY_DONUT = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent MELONADE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent MERINGUE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent MILK_BREAD = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent NUHTELLA_ICE_CREAM = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent POT_PIE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent PRETZEL = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent SUGAR_DONUT = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent SWEETBERRY_DONUT = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent SWEETBERRY_ICE_CREAM = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent SWEETBERRY_JUICE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent SWEETBERRY_PIE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.3f)
+            .alwaysEdible()
+            .snack()
+            .build();
+
+    public static final FoodComponent VANILLA_ICE_CREAM = new FoodComponent.Builder()
             .hunger(4)
             .saturationModifier(0.3f)
             .alwaysEdible()
