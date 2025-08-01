1aa2067f5762248f4e4efe4719c77f7e7c649001
justeriksk <sebo.erik@gmail.com>
2025-08-01 17:38:54 +0200

    update food components for all items

--- src/main/java/net/withrage/item/ModFoodComponents.java
+++ src/main/java/net/withrage/item/ModFoodComponents.java
@@ -6,191 +6,159 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent APPLE_JUICE = new FoodComponent.Builder()
-            .hunger(4)
+            .hunger(6)
             .saturationModifier(0.3f)
             .alwaysEdible()
             .snack()
             .build();
 
     public static final FoodComponent APPLE_PIE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.6f)
             .build();
 
     public static final FoodComponent BERRY_CUPCAKE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.55f)
             .build();
 
     public static final FoodComponent CHICKEN_MUSHROOM_PIE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(10)
+            .saturationModifier(0.85f)
             .build();
 
     public static final FoodComponent CHIPS = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
+            .hunger(6)
+            .saturationModifier(0.4f)
             .snack()
             .build();
 
     public static final FoodComponent CHOCOLATE_CUPCAKE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.45f)
             .build();
 
     public static final FoodComponent CHOCOLATE_DONUT = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.5f)
             .build();
 
     public static final FoodComponent CHOCOLATE_MILK = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
+            .hunger(6)
+            .saturationModifier(0.35f)
             .alwaysEdible()
             .snack()
             .build();
 
     public static final FoodComponent FRIES = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
+            .hunger(8)
+            .saturationModifier(0.5f)
             .snack()
             .build();
 
     public static final FoodComponent GILDED_MELONADE = new FoodComponent.Builder()
-            .hunger(4)
+            .hunger(6)
             .saturationModifier(0.3f)
             .alwaysEdible()
             .snack()
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 1, -1), 100)
             .build();
 
     public static final FoodComponent GLOWBERRY_DONUT = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.5f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 100, 1), 50)
             .build();
 
     public static final FoodComponent GLOWBERRY_ICE_CREAM = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.55f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 100, 1), 50)
             .build();
 
     public static final FoodComponent GLOWBERRY_JUICE = new FoodComponent.Builder()
-            .hunger(4)
+            .hunger(6)
             .saturationModifier(0.3f)
             .alwaysEdible()
             .snack()
+            .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 100, 1), 50)
             .build();
 
     public static final FoodComponent GLOWBERRY_PIE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.5f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 100, 1), 50)
             .build();
 
     public static final FoodComponent JELLY_DONUT = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.8f)
             .build();
 
     public static final FoodComponent MELONADE = new FoodComponent.Builder()
-            .hunger(4)
+            .hunger(6)
             .saturationModifier(0.3f)
             .alwaysEdible()
             .snack()
             .build();
 
     public static final FoodComponent MERINGUE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
+            .hunger(5)
+            .saturationModifier(0.5f)
             .snack()
             .build();
 
     public static final FoodComponent MILK_BREAD = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(8)
+            .saturationModifier(0.4f)
             .build();
 
     public static final FoodComponent NUHTELLA_ICE_CREAM = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.4f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.LUCK, 800, 1), 60)
             .build();
 
     public static final FoodComponent POT_PIE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(10)
+            .saturationModifier(0.85f)
             .build();
 
     public static final FoodComponent PRETZEL = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
+            .hunger(7)
+            .saturationModifier(0.4f)
             .snack()
             .build();
 
     public static final FoodComponent SUGAR_DONUT = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.5f)
             .build();
 
     public static final FoodComponent SWEETBERRY_DONUT = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.5f)
             .build();
 
     public static final FoodComponent SWEETBERRY_ICE_CREAM = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.4f)
             .build();
 
     public static final FoodComponent SWEETBERRY_JUICE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
+            .hunger(6)
+            .saturationModifier(0.2f)
             .alwaysEdible()
             .snack()
             .build();
 
     public static final FoodComponent SWEETBERRY_PIE = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.5f)
             .build();
 
     public static final FoodComponent VANILLA_ICE_CREAM = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(0.3f)
-            .alwaysEdible()
-            .snack()
+            .hunger(7)
+            .saturationModifier(0.4f)
             .build();
 }
